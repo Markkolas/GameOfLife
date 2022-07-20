c853589282f09b28a504a959396e76b4e4b66c47
mark.debian <mark@Roxanbian>
2022-07-20 13:25:39 +0200

    Starting the logic

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,1 +1,23 @@
 #This is a implementation of game of life using python
+
+import turtle
+
+#Screen setup
+screen = turtle.Screen()
+screen.setup(width=800, height=600)
+screen.title("Game of life")
+screen.bgcolor("black")
+screen.tracer(0)
+
+cell = turtle.Turtle()
+#cell.shape("square")
+cell.color("white")
+#cell.shapesize(stretch_wid=5, stretch_len=5)
+
+#Initialize turtles
+"""
+cells = []
+for y in range(600):
+    for x in range(800):
+        holder = []
+"""
